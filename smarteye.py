73809e34d3f20de3decfe61de3ae38369ffbba7e
vinhngooo <70963290+vinhngooo@users.noreply.github.com>
2026-05-26 02:58:59 +0700

    Implement real-time object detection with webcam
    Added webcam functionality for real-time object detection and display.

--- smarteye.py
+++ smarteye.py
@@ -1,14 +1,40 @@
-
+import cv2
 from ultralytics import YOLO
 
+# Load model
 model = YOLO("yolov8n.pt")
 
-results = model('test2.jpg')
+# Mở webcam
+cap = cv2.VideoCapture(0)
 
-for box in results[0].boxes:
+while True:
 
-    cls_id = int(box.cls[0])
+    ret, frame = cap.read()
 
-    name = model.names[cls_id]
+    if not ret:
+        break
 
-    print(name)
+    # Detect
+    results = model(frame)
+
+    # Lấy tên object
+    for box in results[0].boxes:
+
+        cls_id = int(box.cls[0])
+
+        name = model.names[cls_id]
+
+        print(name)
+
+    # Vẽ bounding box
+    annotated = results[0].plot()
+
+    # Hiển thị
+    cv2.imshow("YOLOv8", annotated)
+
+    # Nhấn q để thoát
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
